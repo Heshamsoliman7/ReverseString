477707efeda414b018d385a013c55363cb371d44
HeshamSolo <hesham.soliman@flothers.com>
2023-10-06 17:36:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ReverseString
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string input = "Hello, World!";
+            string reversed = "";
+
+            for (int i = input.Length -1 ; i >= 0; i--)
+            {
+                reversed += input[i];
+            }
+
+            Console.WriteLine(reversed);
+        }
+    }
+
+}
